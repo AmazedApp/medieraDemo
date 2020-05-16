3a3b7f969a6bdf48a3d72ccb87e46c7ac03c837e
AmazedApp <appamazed@gmail.com>
2020-05-17 01:11:54 +0545

    5/17/2020

---
+++ app.js
@@ -0,0 +1,12 @@
+const express=require('express');
+const app=express();
+
+app.get('/',(req,res)=>{
+    console.log("It is working");
+    res.send("Hello its me amazed");
+});
+
+
+app.listen(3000,()=>{
+    console.log("Your server is runnig on port 3000");
+});
